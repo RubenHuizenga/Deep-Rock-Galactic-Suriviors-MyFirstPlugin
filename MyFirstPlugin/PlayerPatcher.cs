e003f030231f5abd1b484a10a49c7988d27c855d
Ruben Huizenga <RubenHuizenga@proton.me>
2024-02-25 15:07:02 +0100

    Initial commit

---
+++ MyFirstPlugin/PlayerPatcher.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using System;
+using Object = UnityEngine.Object;
+
+namespace MyFirstPlugin
+{
+    public static class PlayerPatcher
+    {
+        private static Player player;
+
+        [HarmonyPatch(typeof(Player), "OnExitDropPod")]
+        [HarmonyPrefix] // Prefix makes sure the patch is ran before the original method
+        public static void OnExitDropPodLogger()
+        {
+            player = Object.FindObjectOfType<Player>(); // Find the player object
+            
+            Plugin.Log.LogInfo($"{DateTime.Now} - {player.CurrentClass.DisplayName} Exited Droppod!");
+        }
+
+        [HarmonyPatch(typeof(Player), "OnEnterDropPod")]
+        [HarmonyPrefix]
+        public static void OnEnterDropPodLogger()
+        {
+            Plugin.Log.LogInfo($"{DateTime.Now} - {player.CurrentClass.DisplayName} Entered Droppod!");
+        }
+    }
+}
---
+++ MyFirstPlugin/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using FundayFactory.Audio;
+using FundayFactory.Audio.Data;
+using HarmonyLib;
+
+namespace MyFirstPlugin;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    // Use a single logger for the entire plugin
+    internal new static ManualLogSource Log;
+
+    public override void Load()
+    {
+        Log = base.Log;
+
+        Log.LogInfo($"{DateTime.Now} - Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        
+        Harmony.CreateAndPatchAll(typeof(PlayerPatcher));
+    }
+}
